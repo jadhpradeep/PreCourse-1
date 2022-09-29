9a0f024350eb3b3b70bc4465f405dac355f88fa9
Pradeep Jadhav <114684981+jadhpradeep@users.noreply.github.com>
2022-09-29 19:17:51 -0400

    Excercise_1 change

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,4 +1,7 @@
-class Stack { 
+// Time Complexity : O(1) for push/pop/peek
+// Space Complexity : O(1) for push/pop/peek
+
+class StackCustom { 
     //Please read sample.java file before starting.
   //Kindly include Time and Space complexity at top of each file
     static final int MAX = 1000; 
@@ -7,29 +10,53 @@ class Stack {
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        //Write your code here
+        if(top ==-1)
+         System.out.println(" Stack is empty");  
+        else
+         System.out.println(" Stack is not empty");   
+        return top ==-1;
     } 
 
-    Stack() 
+    StackCustom() 
     { 
         //Initialize your constructor 
+      top =-1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        
+        if(top == MAX-1){
+            System.out.println("Stack is already full");
+            return false;
+        }else{                
+         a[++top] = x;   
+            return true;
+        }
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if(top == -1){
+            System.out.println("stack underflow");
+            return 0;
+        }
+        int result = a[top];
+        a[top--]=0;        
+        return result;
     } 
   
     int peek() 
     { 
         //Write your code here
+        if(isEmpty())
+        return 0;
+        return a[top];
     } 
 } 
   
@@ -37,10 +64,41 @@ class Stack {
 class Main { 
     public static void main(String args[]) 
     { 
-        Stack s = new Stack(); 
+        StackCustom s = new StackCustom(); 
+        s.isEmpty();
         s.push(10); 
-        s.push(20); 
-        s.push(30); 
+        
+        System.out.println("after push 10");
+        for(int i=0;i<=s.top;i++)
+        System.out.println(s.a[i]);
+        
+        s.isEmpty();
+        s.push(20);
+        
+        System.out.println("after push 20");
+        for(int i=0;i<=s.top;i++)
+        System.out.println(s.a[i]);
+        
+        s.isEmpty();
+        s.push(30);
+        
+        System.out.println("after push 30");
+        for(int i=0;i<= s.top;i++)
+        System.out.println(s.a[i]);
+        
+        s.isEmpty();
         System.out.println(s.pop() + " Popped from stack"); 
+        
+        System.out.println("after pop");
+        for(int i=0;i<= s.top;i++)
+        System.out.println(s.a[i]);
+        
+         System.out.println(s.peek() + " Peek from stack"); 
+        
+        System.out.println("after peek");
+        for(int i=0;i<= s.top;i++)
+        System.out.println(s.a[i]);
+        
+        
     } 
 }
